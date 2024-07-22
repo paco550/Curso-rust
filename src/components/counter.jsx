0686a9b2050a95576585524de185f4f220e2e014
paco550 <pacofb70@gmail.com>
2024-07-22 13:20:02 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/counter.jsx
    	modified:   src/components/counter.module.css
    	modified:   src/content/docs/index.mdx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import './counter.module.css';
+import styles from './counter.module.css';
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h2>Contador: {count}</h2>
-      <div className="button-container">
-        <button className="button" onClick={() => setCount(count - 1)}>-</button>
-        <button className="button" onClick={() => setCount(count + 1)}>+</button>
+      <div className={styles['button-container']}>
+        <button className={styles.button} onClick={() => setCount(count - 1)}>-</button>
+        <button className={styles.button} onClick={() => setCount(count + 1)}>+</button>
       </div>
     </div>
   );
